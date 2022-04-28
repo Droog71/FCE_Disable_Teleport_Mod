c96d5ed64c9f2cfa85fbe7d2f1cf51f3c4c9be6e
Droog71 <71droog71@gmail.com>
2022-04-27 23:24:54 -0400

    initial commit

---
+++ TeleportCommand.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+
+[HarmonyPatch(typeof(ConsoleCommands), "Teleport")]
+public class TeleportCommand
+{
+    public static bool Prefix()
+    {
+        if (NetworkManager.instance.mClientThread != null)
+        {
+            Player player = NetworkManager.instance.mClientThread.mPlayer;
+            if (player != null)
+            {
+                if (player.mBuildPermission == eBuildPermission.Admin)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
